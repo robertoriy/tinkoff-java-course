c24291855307019dd5ea9b4329b80f17727c34be
robertoriy <holdenxhume@gmail.com>
2023-10-13 03:01:34 +0700

    homework-1: add solution for task-4

--- src/main/java/edu/hw1/task4/BrokenString.java
+++ src/main/java/edu/hw1/task4/BrokenString.java
@@ -5,6 +5,21 @@ public final class BrokenString {
     }
 
     public static String fixString(String input) {
-        return "hello";
+        if (input == null) {
+            throw new IllegalArgumentException("Null input");
+        }
+        if (input.length() <= 1) {
+            return input;
+        }
+
+        char[] charArray = input.toCharArray();
+
+        for (int i = 0; i < charArray.length - 1; i += 2) {
+            char temp = charArray[i];
+            charArray[i] = charArray[i + 1];
+            charArray[i + 1] = temp;
+        }
+
+        return String.valueOf(charArray);
     }
 }
