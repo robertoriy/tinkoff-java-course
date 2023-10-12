3c2a1f0a52a3ca75e07f6394b674c35740692b74
robertoriy <holdenxhume@gmail.com>
2023-10-13 03:05:01 +0700

    homework-1: add template*-solution {need refactoring} for task-3

--- src/main/java/edu/hw1/task3/NestedArray.java
+++ src/main/java/edu/hw1/task3/NestedArray.java
@@ -4,7 +4,37 @@ public final class NestedArray {
     private NestedArray() {
     }
 
-    public static boolean isNested(int[] a, int[] b) {
-        return false;
+    public static boolean isNestable(int[] a, int[] b) {
+        if (!isValidArray(a) || !isValidArray(b)) {
+            return false;
+        }
+
+        ArrayProperties aContent = findMinAndMax(a);
+        ArrayProperties bContent = findMinAndMax(b);
+
+        return (aContent.min > bContent.min) && (aContent.max < bContent.max);
     }
+
+    private static boolean isValidArray(int[] array) {
+        return (array != null && array.length > 0);
+    }
+
+    private static ArrayProperties findMinAndMax(int[] array) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int value : array) {
+            if (value < min) {
+                min = value;
+            }
+            if (value > max) {
+                max = value;
+            }
+        }
+        return new ArrayProperties(min, max);
+    }
+
+    record ArrayProperties(int min, int max) {
+    }
+
 }
