5537a567724870b12e5b87af6bdc9b4313d3b02c
robertoriy <holdenxhume@gmail.com>
2023-10-13 03:05:34 +0700

    homework-1: add template*-solution {need refactoring} for task-5

--- src/main/java/edu/hw1/task5/SpecialPalindrome.java
+++ src/main/java/edu/hw1/task5/SpecialPalindrome.java
@@ -6,6 +6,48 @@ public final class SpecialPalindrome {
     }
 
     public static boolean isPalindromeDescendant(int number) {
+        if (number < 0) {
+            return false;
+        }
+        char[] digits = Integer.toString(number).toCharArray();
+
+        if (isPalindrome(digits)) {
+            return true;
+        }
+
+        while (digits.length > 2) {
+
+            if (digits.length % 2 != 0) {
+                return false;
+            }
+            digits = sumByPairs(digits);
+
+            if (isPalindrome(digits)) {
+                return true;
+            }
+        }
         return false;
     }
+
+    private static boolean isPalindrome(char[] data) {
+        for (int i = 0, j = data.length - 1; i < j; i++, j--) {
+            if (data[i] != data[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static char[] sumByPairs(char[] digits) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < digits.length - 1; i += 2) {
+            int first = Character.getNumericValue(digits[i]);
+            int second = Character.getNumericValue(digits[i + 1]);
+
+            result.append(first + second);
+        }
+        return result.toString().toCharArray();
+    }
+
 }
