43ceac821964adb9aa1752d19143954d2cec52b2
robertoriy <holdenxhume@gmail.com>
2023-10-15 06:58:54 +0700

    homework-1: reformat template code

--- src/main/java/edu/hw1/Main.java
+++ src/main/java/edu/hw1/Main.java
@@ -1,13 +1,12 @@
 package edu.hw1;
 
-import edu.hw1.task1.VideoLength;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public final class Main {
-    public static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     private Main() {
     }
