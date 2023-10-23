e420677d83d890c92c449ff565f2fb570ce9c0ea
robertoriy <holdenxhume@gmail.com>
2023-10-23 15:45:26 +0700

    hangman: add defeat case and give clear names

--- src/test/java/edu/project1/SessionTest.java
+++ src/test/java/edu/project1/SessionTest.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 final class SessionTest {
     @ParameterizedTest
     @DisplayName("Тест на корректное изменение состояния")
-    @MethodSource("provideWordsAndGuessAndState")
+    @MethodSource("provideCorrectStateCase")
     void testState(Word word, char guess, Class<? extends GuessState> expectedStateClass) {
         Session session = new Session(word);
 
@@ -27,7 +27,7 @@ final class SessionTest {
         assertThat(actualState).isInstanceOf(expectedStateClass);
     }
 
-    public static Stream<Arguments> provideWordsAndGuessAndState() {
+    public static Stream<Arguments> provideCorrectStateCase() {
         return Stream.of(
             Arguments.of(new Word("hello"), 'l', Hit.class),
             Arguments.of(new Word("aaaaa"), 'a', Victory.class),
@@ -36,9 +36,31 @@ final class SessionTest {
     }
 
     @ParameterizedTest
-    @DisplayName("Тест на получение индексов каждой буквы")
-    @MethodSource("provideWordsAndGuess")
-    void testGuess(Word word, char guess, String expectedHint) {
+    @DisplayName("Тест на гарантированный проигрыш после заданного количества ошибок")
+    @MethodSource("provideDefeatCase")
+    void testDefeat(Word word, char guess, int maxAllowedMisses) {
+        Session session = new Session(word, maxAllowedMisses);
+        GuessState actualState = null;
+
+        for (int i = 0; i < maxAllowedMisses; i++) {
+            actualState = session.guess(guess);
+        }
+
+        assertThat(actualState).isInstanceOf(Defeat.class);
+    }
+
+    public static Stream<Arguments> provideDefeatCase() {
+        return Stream.of(
+            Arguments.of(new Word("hello"), 'f', 4),
+            Arguments.of(new Word("aaaaa"), 'b', 2),
+            Arguments.of(new Word("first"), 'a', 5)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Тест на открытие нужной буквы в подсказке")
+    @MethodSource("provideHitCase")
+    void testHitGuess(Word word, char guess, String expectedHint) {
         Session session = new Session(word);
 
         session.guess(guess);
@@ -47,7 +69,7 @@ final class SessionTest {
         assertThat(actualHint).isEqualTo(expectedHint);
     }
 
-    public static Stream<Arguments> provideWordsAndGuess() {
+    public static Stream<Arguments> provideHitCase() {
         return Stream.of(
             Arguments.of(new Word("hello"), 'l', "**ll*"),
             Arguments.of(new Word("aabcaad"), 'a', "aa**aa*"),
@@ -64,5 +86,4 @@ final class SessionTest {
 
         assertThat(actualState).isInstanceOf(Defeat.class);
     }
-
 }
