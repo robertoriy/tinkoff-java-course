a5f70f69fa7019d89bbc7168d5c716cf4e1ce32c
robertoriy <holdenxhume@gmail.com>
2023-11-05 05:37:25 +0700

    homework-4: update validation error

--- src/main/java/edu/hw4/ValidationError.java
+++ src/main/java/edu/hw4/ValidationError.java
@@ -2,10 +2,48 @@ package edu.hw4;
 
 import java.util.stream.Stream;
 
-public record ValidationError(AnimalError error) {
+public record ValidationError(String field, AnimalError error) {
+    @SuppressWarnings("checkstyle:MagicNumber")
+    public static Stream<ValidationError> errors(Animal animal) {
+        String[] fields = Animal.getFieldNames();
+        Stream.Builder<ValidationError> errorStream = Stream.builder();
+
+        addStringError(animal.name(), fields[0], errorStream);
+
+        addNullError(animal.type(), fields[1], errorStream);
+        addNullError(animal.sex(), fields[2], errorStream);
+
+        addNegativeValueError(animal.age(), fields[3], errorStream);
+        addNegativeValueError(animal.height(), fields[4], errorStream);
+        addNegativeValueError(animal.weight(), fields[5], errorStream);
+
+        return errorStream.build();
+    }
+
+    private static void addStringError(String fieldValue, String fieldName, Stream.Builder<ValidationError> errors) {
+        if (fieldValue == null) {
+            errors.add(new ValidationError(fieldName, AnimalError.NULL_VALUE));
+        } else if (fieldValue.isBlank()) {
+            errors.add(new ValidationError(fieldName, AnimalError.EMPTY_STRING));
+        }
+    }
+
+    private static void addNullError(Object fieldValue, String fieldName, Stream.Builder<ValidationError> errors) {
+        if (fieldValue == null) {
+            errors.add(new ValidationError(fieldName, AnimalError.NULL_VALUE));
+        }
+    }
+
+    private static void addNegativeValueError(int value, String fieldName, Stream.Builder<ValidationError> errors) {
+        if (value < 0) {
+            errors.add(new ValidationError(fieldName, AnimalError.NEGATIVE_VALUE));
+        }
+    }
+
     enum AnimalError {
-        EMPTY_NAME("there is no name"),
-        NEGATIVE_ATTRIBUTES("attributes must be positive");
+        EMPTY_STRING("Must be not empty"),
+        NEGATIVE_VALUE("Must be positive"),
+        NULL_VALUE("Must be not null");
 
         private final String description;
 
@@ -16,16 +54,5 @@ public record ValidationError(AnimalError error) {
         public String getDescription() {
             return description;
         }
-        }
-
-    public static Stream<ValidationError> findErrors(Animal animal) {
-        Stream.Builder<ValidationError> errorStream = Stream.builder();
-        if (animal.name().isBlank()) {
-            errorStream.add(new ValidationError(AnimalError.EMPTY_NAME));
-        }
-        if (animal.age() < 0 || animal.height() < 0 || animal.weight() < 0) {
-            errorStream.add(new ValidationError(AnimalError.NEGATIVE_ATTRIBUTES));
-        }
-        return errorStream.build();
     }
 }
