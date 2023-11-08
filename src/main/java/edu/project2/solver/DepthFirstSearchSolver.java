56ec4b72a628be8e75405dcfd9d60c33404893cc
robertoriy <holdenxhume@gmail.com>
2023-11-08 21:46:05 +0700

    maze: refactored Solver - removed recursion

--- src/main/java/edu/project2/solver/DepthFirstSearchSolver.java
+++ src/main/java/edu/project2/solver/DepthFirstSearchSolver.java
@@ -3,51 +3,61 @@ package edu.project2.solver;
 import edu.project2.model.Cell;
 import edu.project2.model.Coordinate;
 import edu.project2.model.Maze;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
 
 public class DepthFirstSearchSolver implements Solver {
-    private Maze maze;
-    private boolean[][] visitedCells;
-    private List<Coordinate> path;
+    private static final int NO_STEP = 0;
+    private static final int STEP = 1;
 
     @Override
     public List<Coordinate> solve(Maze maze, Coordinate start, Coordinate end) {
         if (isInvalidCoordinates(maze, start, end)) {
             throw new IllegalArgumentException("Invalid coordinates: out of maze or not passage");
         }
-        update(maze);
-        if (!dfs(start, end)) {
-            return Collections.emptyList();
-        }
-        return path;
+        return dfs(maze, start, end);
     }
 
-    private boolean dfs(Coordinate current, Coordinate end) {
-        visitedCells[current.row()][current.column()] = true;
-        path.add(current);
-        if (current.equals(end)) {
-            return true;
-        }
-        List<Coordinate> neighbours = getNeighboursWithPassage(current);
-        for (Coordinate neighbour : neighbours) {
-            if (!visitedCells[neighbour.row()][neighbour.column()] && dfs(neighbour, end)) {
-                return true;
+    private List<Coordinate> dfs(Maze maze, Coordinate start, Coordinate end) {
+        boolean[][] visitedCells = new boolean[maze.getHeight()][maze.getWidth()];
+        Deque<Coordinate> stack = new ArrayDeque<>();
+
+        visitedCells[start.row()][start.column()] = true;
+        stack.push(start);
+
+        while (!stack.isEmpty()) {
+            Coordinate current = stack.peek();
+            if (current.equals(end)) {
+                return new ArrayList<>(stack).reversed();
+            }
+            List<Coordinate> neighbours = getNeighboursWithPassage(maze, current);
+
+            boolean found = false;
+            for (Coordinate neighbour : neighbours) {
+                if (!visitedCells[neighbour.row()][neighbour.column()]) {
+                    visitedCells[neighbour.row()][neighbour.column()] = true;
+                    stack.push(neighbour);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                stack.pop();
             }
         }
-        path.removeLast();
-        return false;
+        return Collections.emptyList();
     }
 
-    private void update(Maze maze) {
-        this.maze = maze;
-        visitedCells = new boolean[maze.getHeight()][maze.getWidth()];
-        path = new ArrayList<>();
-    }
-
-    private List<Coordinate> getNeighboursWithPassage(Coordinate current) {
-        final int[][] neighboursCoordinates = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private List<Coordinate> getNeighboursWithPassage(Maze maze, Coordinate current) {
+        final int[][] neighboursCoordinates = {
+            {STEP, NO_STEP},
+            {-STEP, NO_STEP},
+            {NO_STEP, STEP},
+            {NO_STEP, -STEP}
+        };
         List<Coordinate> neighbours = new ArrayList<>();
 
         for (int[] neighbour : neighboursCoordinates) {
