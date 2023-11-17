7f99460853936f7cd48ba8d811f6b3185980db52
robertoriy <holdenxhume@gmail.com>
2023-11-18 06:15:08 +0700

    homework-6: refactored task4

--- src/main/java/edu/hw6/task4/NiceWriter.java
+++ src/main/java/edu/hw6/task4/NiceWriter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
@@ -13,6 +14,7 @@ import java.util.zip.Adler32;
 import java.util.zip.CheckedOutputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import static java.nio.file.StandardOpenOption.APPEND;
 
 public final class NiceWriter {
     private static final Logger LOGGER = LogManager.getLogger();
@@ -21,25 +23,24 @@ public final class NiceWriter {
     private NiceWriter() {
     }
 
-    public static boolean write(Path path) {
-        return write(path, DEFAULT_CONTENT);
+    public static void println(Path path) {
+        println(path, DEFAULT_CONTENT);
     }
 
-    public static boolean write(Path path, String content) {
+    public static void println(Path path, String content) {
         createPathIfNotExist(path);
 
-        try (OutputStream fos = Files.newOutputStream(path);
+        try (OutputStream fos = Files.newOutputStream(path, APPEND);
              CheckedOutputStream cos = new CheckedOutputStream(fos, new Adler32());
              BufferedOutputStream bos = new BufferedOutputStream(cos);
              OutputStreamWriter osw = new OutputStreamWriter(bos, StandardCharsets.UTF_8);
              PrintWriter printWriter = new PrintWriter(osw)) {
             LOGGER.info("NiceWriter is built");
             printWriter.println(content);
-        } catch (IOException e) {
-            LOGGER.error("Something went wrong - {}", e.getMessage());
-            return false;
+        } catch (IOException error) {
+            LOGGER.error("Something went wrong - {}", error.getMessage());
+            throw new UncheckedIOException(error);
         }
-        return true;
     }
 
     private static void createPathIfNotExist(Path path) {
